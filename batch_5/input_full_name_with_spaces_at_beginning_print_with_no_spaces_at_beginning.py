b041dbe49a516ec6dc3110933a87ac6418f8f94a
my name <you@example.com>
2025-03-20 22:26:22 +0800

    Input name, Delete unnecessary spaces, and print result

--- batch_5/input_full_name_with_spaces_at_beginning_print_with_no_spaces_at_beginning.py
+++ batch_5/input_full_name_with_spaces_at_beginning_print_with_no_spaces_at_beginning.py
@@ -1,3 +1,8 @@
 # ask input for full name with spaces at the beginning
+full_name = input("Input full name with spaces at beginning: ")
 # remove spaces from beginning
+
+fixed_full_name = full_name.lstrip()
+
 # print the result
+print(f"The inputted name is {fixed_full_name}")
