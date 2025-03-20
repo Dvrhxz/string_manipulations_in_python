1194be6c2c20928a43f271fc373544755d2b7bcc
my name <you@example.com>
2025-03-20 22:45:40 +0800

    input, format number, and print results

--- batch_5/input_num_0_to_1000_print_num_as_6_digits_format_fill_with_0_if_needed.py
+++ batch_5/input_num_0_to_1000_print_num_as_6_digits_format_fill_with_0_if_needed.py
@@ -1,3 +1,8 @@
 # ask for input 0-1000
+number = int(input("enter a number: "))
+
 # format the number
+formatted_number = f"{number:06d}"
+
 # print the number
+print(f"Your number is : {formatted_number}")
