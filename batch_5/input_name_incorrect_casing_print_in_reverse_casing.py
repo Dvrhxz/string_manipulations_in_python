b4a0bac10283cc3024f9d7583b1c72eeda228b3a
my name <you@example.com>
2025-03-21 20:19:18 +0800

    prog 6 input name, reverse the casing, print results

--- batch_5/input_name_incorrect_casing_print_in_reverse_casing.py
+++ batch_5/input_name_incorrect_casing_print_in_reverse_casing.py
@@ -1,3 +1,8 @@
 # ask for full name in incorrect casing
+full_name = input("input your full name in incorrect casing:")
+
 # reverse the casing
+reverse_name_casing = full_name.swapcase()
+
 # print result
+print(f"Output: {reverse_name_casing}")
