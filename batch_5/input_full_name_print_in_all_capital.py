f51802fab6aee7c560eb1618adbc9844cb5a09a0
my name <you@example.com>
2025-03-21 20:02:59 +0800

    input name, capitalize, and printing of result

--- batch_5/input_full_name_print_in_all_capital.py
+++ batch_5/input_full_name_print_in_all_capital.py
@@ -1,3 +1,8 @@
 # ask to input full name
+full_name = input("Input your full name: ")
+
 # turn into capitals
+capitalized_name = full_name.upper()
+
 # print results
+print(f"Output: {capitalized_name}")
