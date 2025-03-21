5a571345d640df7415a274b09236f75ed7328f10
my name <you@example.com>
2025-03-21 20:39:36 +0800

    prog 10 input full name in incorrect casing, convert to snake case, print result, corrected comment

--- batch_5/input_name_incorrect_casing_print_in_snake_case.py
+++ batch_5/input_name_incorrect_casing_print_in_snake_case.py
@@ -1,3 +1,8 @@
-# ask for full name in incorrect spacing
+# ask for full name in incorrect casing
+full_name = input("Input full name in incorrect spacing: ")
+
 # convert to snake case
+snake_case_name = full_name.lower().replace(" ", "_")
+
 # print result
+print(f"Output: {snake_case_name}")
