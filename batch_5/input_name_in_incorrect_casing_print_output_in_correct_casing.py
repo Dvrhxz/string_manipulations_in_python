bc336aabca7377f22770655a471b350449e05803
my name <you@example.com>
2025-03-21 20:13:19 +0800

    prog 5 input name, fixed casing, print results

--- batch_5/input_name_in_incorrect_casing_print_output_in_correct_casing.py
+++ batch_5/input_name_in_incorrect_casing_print_output_in_correct_casing.py
@@ -1,3 +1,8 @@
 # ask for full name in incorrect casing
+full_name = input("input your full name in incorrect casing:")
+
 # correct the casing
+fixed_full_name = full_name.title()
+
 # print result
+print(f"Output: {fixed_full_name}")
