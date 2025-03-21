027bcd762b21729bd54b51ddfc970a35f67bcfd3
my name <you@example.com>
2025-03-21 20:29:55 +0800

    prog 8 input name, count each character, print the amount counted

--- batch_5/input_full_name_print_number_of_characters.py
+++ batch_5/input_full_name_print_number_of_characters.py
@@ -1,3 +1,8 @@
 # ask for full name
+full_name = input("Input your full name: ")
+
 # count the amount of characters
+character_count = len(full_name)
+
 # print the amount
+print(f"Output: {character_count}")
