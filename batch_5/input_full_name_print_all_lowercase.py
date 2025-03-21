cc200beea2fde738b7111b2676cda6757a110194
my name <you@example.com>
2025-03-21 20:07:50 +0800

    input name, turn all into lower case, print results

--- batch_5/input_full_name_print_all_lowercase.py
+++ batch_5/input_full_name_print_all_lowercase.py
@@ -1,3 +1,8 @@
 # ask for full name
+full_name = input("input your full name:")
+
 # turn all into lowercase
+lower_cased_name = full_name.lower()
+
 # print result
+print(f"Output: {lower_cased_name}")
