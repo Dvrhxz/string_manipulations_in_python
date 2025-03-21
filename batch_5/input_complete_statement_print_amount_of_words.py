0b96d8b38ccb17463113cb3501880b004c5fd911
my name <you@example.com>
2025-03-21 20:26:00 +0800

    prog 7 input statement, counting of words, printing amount

--- batch_5/input_complete_statement_print_amount_of_words.py
+++ batch_5/input_complete_statement_print_amount_of_words.py
@@ -1,3 +1,8 @@
 # ask for a statement
+statement = input("Input a complete statement: ")
+
 # count how many words in statement
+word_count = len(statement.split())
+
 # print result
+print(f"Output: {word_count}")
