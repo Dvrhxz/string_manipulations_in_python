06d18b5c559d3fa5c8d676064b275da220c2a146
my name <you@example.com>
2025-03-21 20:34:35 +0800

    prog 9 input name in incorrect casing, convert to pascal case, print the result

--- batch_5/input_name_incorrect_casing_print_in_pascal_case.py
+++ batch_5/input_name_incorrect_casing_print_in_pascal_case.py
@@ -1,3 +1,8 @@
 # ask for full name in incorrect spacing
+full_name = input("Input your full name in incorrect spacing: ")
+
 # convert to pascal case
+pascal_cased_name = full_name.title().replace(" ", "")
+
 # print result
+print(f"Output: {pascal_cased_name}")
